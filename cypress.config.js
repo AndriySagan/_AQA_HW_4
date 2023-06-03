2c65a4344ac7b46175771efd17e71323e8916773
AndriySagan <sagan.a.v.g@gmail.com>
2023-06-04 00:58:50 +0300

    Додав налаштування на збереження відео 777

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  video: true, //вмикаю запис відео
+  videoCompression: 16, // компресія(стискання об'єму відео за рахунок зменшення якості) за замовчуванням стоїть 32
+  videoUploadOnPasses: false, // false, відео буде завантажуватися тільки в разі збою тесту або його неуспішного проходження.
 });
