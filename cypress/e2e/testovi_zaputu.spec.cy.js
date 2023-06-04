ac8b3883625b848583ed7fa086ae1c77d3c1b178
AndriySagan <sagan.a.v.g@gmail.com>
2023-06-04 03:02:19 +0300

    трохи пошаманив11

--- cypress/e2e/testovi_zaputu.spec.cy.js
+++ cypress/e2e/testovi_zaputu.spec.cy.js
@@ -27,3 +27,65 @@ describe("#9 Запит методом GET на сайт https://tenders.guru/pl
     }
   });
 });
+
+describe("#9.1 Запит методом GET на сайт https://tenders.guru/pl/api ", () => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  it("Отримайте розширений набір даних для тендеру, отримання id та description", () => {
+    for (let index = 0; index < 2; index++) {
+      const randomPage = getRandomInt(5830);
+
+      const request = {
+        method: "GET",
+        url: "https://tenders.guru/api/pl/tenders/",
+        // page_number: randomPage,
+        failOnStatusCode: false,
+      };
+
+      cy.request(request).then((response) => {
+        assert.isTrue(response.status == 200);
+        const id = response.body.data[0].id; // Отримання id з відповіді
+        cy.log(`page_number: ${randomPage}`);
+        cy.log("request", request);
+        cy.log("response", response);
+        cy.log("id", id);
+        cy.log("page_number", response.body.page_number);
+        // cy.log("data. id", response.body.data[1].id);
+        // cy.log("description:", response.body.data[page_number].description);
+      });
+    }
+  });
+});
+
+describe("#9.2 Запит методом GET на сайт https://tenders.guru/pl/api ", () => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  it("Отримайте розширений набір даних для тендеру, отримання id та description", () => {
+    for (let index = 0; index < 2; index++) {
+      const randomPage = getRandomInt(5830);
+
+      const request = {
+        method: "GET",
+        url: "https://tenders.guru/api/pl/tenders/",
+        // page_number: randomPage,
+        failOnStatusCode: false,
+      };
+
+      cy.request(request).then((response) => {
+        assert.isTrue(response.status == 200);
+        const id = response.body.data[0].id; // Отримання id з відповіді
+        cy.log(`page_number: ${randomPage}`);
+        cy.log("request", request);
+        cy.log("response", response);
+        cy.log("id", id);
+        cy.log("page_number", response.body.page_number);
+        // cy.log("data. id", response.body.data[1].id);
+        // cy.log("description:", response.body.data[page_number].description);
+      });
+    }
+  });
+});
