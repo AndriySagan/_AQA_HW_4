81e374e4f95ad4def73aa89350ad742e258b705b
AndriySagan <sagan.a.v.g@gmail.com>
2023-06-04 03:24:34 +0300

    3334

--- cypress/e2e/testovi_zaputu.spec.cy.js
+++ cypress/e2e/testovi_zaputu.spec.cy.js
@@ -89,3 +89,17 @@ describe("#9.2 Запит методом GET на сайт https://tenders.guru/
     }
   });
 });
+
+describe("Негативний сценарій запиту ", () => {
+  const request = {
+    method: "GET",
+    url: "https://httpbin.org/post",
+    failOnStatusCode: false,
+  };
+
+  it("response code should be 405", () => {
+    cy.request(request).then((response) => {
+      assert.equal(405, response.status);
+    });
+  });
+});
